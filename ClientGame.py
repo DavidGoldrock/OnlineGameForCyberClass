083c4db7f1500c94197b2eca92e82eaca2938466
David Goldrock <gaming131213@gmail.com>
2022-11-11 20:30:46 +0200

    added some gui

--- ClientGame.py
+++ ClientGame.py
@@ -4,7 +4,7 @@ import Client
 import pygame
 from pygame.locals import *
 from pygame.draw import *
-
+import pygame_gui
 import Definitions
 from Definitions import *
 from Protocol import RequestType
@@ -26,10 +26,9 @@ defaultHeight = 640
 window = pygame.display.set_mode([defaultWidth, defaultHeight], RESIZABLE)
 clock = pygame.time.Clock()
 running = True
+hub = True
 # TODO menu, button for creating games, list of current games to join, textbox to enter password, pause menu (with
 #  compatibility with server)
-
-Cardinality = Client.sendAndRecv(RequestType.CREATE_GAME, {"name": "chen", "password": "none"}).value
 
 
 # Cardinality = Client.send(RequestType.JOIN_GAME, {"name": "chen", "password": None}).value
@@ -42,6 +41,32 @@ def textBlock(text: str, x: float, y: float, size: int, color: tuple):
 	window.blit(screenText, (x, y))
 
 
+manager = pygame_gui.UIManager(getScreenSize())
+CreateGameButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0, getScreenSize()[1] - 50), (130, 50)),
+                                            text='Create Game',
+                                            manager=manager)
+JoinGameButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((180, getScreenSize()[1] - 50), (130, 50)),
+                                            text='Join Game',
+                                            manager=manager)
+while hub:
+	timeDelta = clock.tick(FPS) / 1000
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			hub = False
+			running = False
+		if event.type == pygame_gui.UI_BUTTON_PRESSED:
+			if event.ui_element == CreateGameButton:
+				Cardinality = Client.sendAndRecv(RequestType.CREATE_GAME, {"name": "chen", "password": "none"}).value
+				print(Cardinality)
+				Client.sendAndRecv(RequestType.RETRIEVE_GAMES).print(True)
+				CreateGameButton.kill()
+				JoinGameButton.kill()
+				hub = False
+		manager.process_events(event)
+	manager.update(timeDelta)
+	manager.draw_ui(window)
+	pygame.display.flip()
+	window.fill((0, 0, 0))
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -49,8 +74,8 @@ while running:
 	keys = pygame.key.get_pressed()
 	clock.tick(FPS)
 	# [Rendering]
-	Client.send(RequestType.SET_Y, min(getRelativePosition()[1],1-Definitions.PLAYER_HEIGHT))
-	print(min(getRelativePosition()[1],1-Definitions.PLAYER_HEIGHT))
+	Client.send(RequestType.SET_Y, min(getRelativePosition()[1], 1 - Definitions.PLAYER_HEIGHT))
+	print(min(getRelativePosition()[1], 1 - Definitions.PLAYER_HEIGHT))
 	gameVars = Client.sendAndRecv(RequestType.GET_GAME_VARS).value
 	screenSize = getScreenSize()
 	if Cardinality == 1:
@@ -59,13 +84,16 @@ while running:
 			DISTANCE_FROM_WALL * screenSize[0], opponentY, PLAYER_WIDTH * screenSize[0],
 			PLAYER_HEIGHT * screenSize[1]))
 		rect(window, (255, 255, 255), (
-			(1 - DISTANCE_FROM_WALL - PLAYER_WIDTH) * screenSize[0], min(getRelativePosition()[1],1-Definitions.PLAYER_HEIGHT) * getScreenSize()[1],
+			(1 - DISTANCE_FROM_WALL - PLAYER_WIDTH) * screenSize[0],
+			min(getRelativePosition()[1], 1 - Definitions.PLAYER_HEIGHT) * getScreenSize()[1],
 			PLAYER_WIDTH * screenSize[0],
 			PLAYER_HEIGHT * screenSize[1]))
 	else:
 		opponentY = gameVars.player2y * getScreenSize()[1]
 		rect(window, (255, 255, 255), (
-			DISTANCE_FROM_WALL * screenSize[0], min(getRelativePosition()[1],1-Definitions.PLAYER_HEIGHT) * getScreenSize()[1], PLAYER_WIDTH * screenSize[0],
+			DISTANCE_FROM_WALL * screenSize[0],
+			min(getRelativePosition()[1], 1 - Definitions.PLAYER_HEIGHT) * getScreenSize()[1],
+			PLAYER_WIDTH * screenSize[0],
 			PLAYER_HEIGHT * screenSize[1]))
 		rect(window, (255, 255, 255), (
 			(1 - DISTANCE_FROM_WALL - PLAYER_WIDTH) * screenSize[0], opponentY, PLAYER_WIDTH * screenSize[0],
